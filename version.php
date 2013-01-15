0e693a12a48ac08fe2c72d8e405f4f781a29f605
betollaque <betollaque@gmail.com>
2013-01-15 16:49:38 -0500

    Initial commit

---
+++ version.php
@@ -0,0 +1,8 @@
+<?php
+defined('MOODLE_INTERNAL') || die;
+
+$plugin->version   = 2012012001; // The current module version (Date: YYYYMMDDXX)
+$plugin->requires  = 2012012001; // Requires this Moodle version
+$plugin->release = '0.1 (Build: 2012012001)';
+$plugin->component = 'theme_moodle2mobile';
+$plugin->maturity  = MATURITY_STABLE;
