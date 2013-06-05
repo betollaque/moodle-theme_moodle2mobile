5f18a632925d78ea4e98edac829d529d84477af6
betollaque <betollaque@gmail.com>
2013-06-05 15:16:31 -0500

    change version

--- version.php
+++ version.php
@@ -1,8 +1,8 @@
 <?php
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2013053001; // The current module version (Date: YYYYMMDDXX)
+$plugin->version   = 2013053002; // The current module version (Date: YYYYMMDDXX)
 $plugin->requires  = 2010112400; // Requires this Moodle version
-$plugin->release = '1.0 (Build: 2013053001)';
+$plugin->release = '1.1 (Build: 2013053002)';
 $plugin->component = 'theme_moodle2mobile';
 $plugin->maturity  = MATURITY_STABLE;
