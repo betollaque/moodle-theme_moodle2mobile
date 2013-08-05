f0346d5b2348e6ae8c0aef7e6d2b4ff355eafdeb
betollaque <betollaque@gmail.com>
2013-08-05 11:05:27 -0500

    css changes, added an alternative general.php layout

--- version.php
+++ version.php
@@ -1,8 +1,8 @@
 <?php
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2013053002; // The current module version (Date: YYYYMMDDXX)
+$plugin->version   = 2013080501; // The current module version (Date: YYYYMMDDXX)
 $plugin->requires  = 2010112400; // Requires this Moodle version
-$plugin->release = '1.1 (Build: 2013053002)';
+$plugin->release = '1.2 (Build: 2013080501)';
 $plugin->component = 'theme_moodle2mobile';
 $plugin->maturity  = MATURITY_STABLE;
